0b58bbfcbd459b5afecf6a94b730f5dc3e47de27
Anastasiia Ivanchenko <iva21sta@gmail.com>
2023-04-19 00:48:37 +0300

    add eslint, refactor

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,48 @@
-(()=>{"use strict";const e=document.querySelector(".inputs"),t=document.querySelector(".form"),r=document.querySelector(".answer-text");window.addEventListener("DOMContentLoaded",(function(){e.querySelectorAll(".input").forEach((e=>e.value=""))})),t.addEventListener("submit",(t=>{t.preventDefault(),function(e){let t="";t=1==e.length?`There is one root: ${e[0]}.`:2==e.length?`There are two roots: ${e[0]} and ${e[1]}.`:"There are no real roots.",r.innerHTML=t}(function(e,t,r){let o=[];const n=Math.pow(t,2)-4*e*r;return n>=0&&o.push(((-t+Math.sqrt(n))/(2*e)).toFixed(1)),n>0&&o.push(((-t-Math.sqrt(n))/(2*e)).toFixed(1)),o}(e.querySelector("#coef-a").value,e.querySelector("#coef-b").value,e.querySelector("#coef-c").value))})),e.addEventListener("input",(({target:e})=>{var t;"coef-a"==e.id?(t=e.value,document.querySelector('[data-id="coef-a"]').innerHTML=""==t||isNaN(t)?"a":t):function(e,t,r){const o=document.querySelector(`[data-id="${t}"]`),n=document.querySelector(`[data-operator="${t}"]`);o.innerHTML=""==e||isNaN(e)?r:Math.abs(e),n.innerHTML=e<0?"-":"+"}(e.value,e.id,e.dataset.coef)}))})();
+(() => {
+  const e = document.querySelector('.inputs');
+  const t = document.querySelector('.form');
+  const r = document.querySelector('.answer-text');
+  window.addEventListener('DOMContentLoaded', () => {
+    e.querySelectorAll('.input').forEach((e) => (e.value = ''));
+  }),
+    t.addEventListener('submit', (t) => {
+      t.preventDefault(),
+        (function (e) {
+          let t = '';
+          (t =
+            e.length == 1
+              ? `There is one root: ${e[0]}.`
+              : e.length == 2
+              ? `There are two roots: ${e[0]} and ${e[1]}.`
+              : 'There are no real roots.'),
+            (r.innerHTML = t);
+        })(
+          (function (e, t, r) {
+            const o = [];
+            const n = t ** 2 - 4 * e * r;
+            return (
+              n >= 0 && o.push(((-t + Math.sqrt(n)) / (2 * e)).toFixed(1)),
+              n > 0 && o.push(((-t - Math.sqrt(n)) / (2 * e)).toFixed(1)),
+              o
+            );
+          })(
+            e.querySelector('#coef-a').value,
+            e.querySelector('#coef-b').value,
+            e.querySelector('#coef-c').value
+          )
+        );
+    }),
+    e.addEventListener('input', ({ target: e }) => {
+      let t;
+      e.id == 'coef-a'
+        ? ((t = e.value),
+          (document.querySelector('[data-id="coef-a"]').innerHTML =
+            t == '' || isNaN(t) ? 'a' : t))
+        : (function (e, t, r) {
+            const o = document.querySelector(`[data-id="${t}"]`);
+            const n = document.querySelector(`[data-operator="${t}"]`);
+            (o.innerHTML = e == '' || isNaN(e) ? r : Math.abs(e)),
+              (n.innerHTML = e < 0 ? '-' : '+');
+          })(e.value, e.id, e.dataset.coef);
+    });
+})();
--- src/index.js
+++ src/index.js
@@ -1,57 +1,49 @@
-import {solveEquation} from 'quadratic-roots-function';
+import { solveEquation } from 'quadratic-roots-function';
 
 const inputsContainer = document.querySelector('.inputs');
 const form = document.querySelector('.form');
 const answerField = document.querySelector('.answer-text');
 
-window.addEventListener('DOMContentLoaded', clearInputs);
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const a = inputsContainer.querySelector('#coef-a').value;
-    const b = inputsContainer.querySelector('#coef-b').value;
-    const c = inputsContainer.querySelector('#coef-c').value;
-    showAnswer(solveEquation(a, b, c));
-});
-
 function showAInFormula(value) {
-    const a = document.querySelector('[data-id="coef-a"]');
-    a.innerHTML = value != '' && !isNaN(value) ? value : 'a';
+  const a = document.querySelector('[data-id="coef-a"]');
+  a.innerHTML = value !== '' && !Number.isNaN(value) ? value : 'a';
 }
 
 function showOtherThanAInFormula(value, id, coefficient) {
-    const currentCoef = document.querySelector(`[data-id="${id}"]`);
-    const operator = document.querySelector(`[data-operator="${id}"]`);
+  const currentCoef = document.querySelector(`[data-id="${id}"]`);
+  const operator = document.querySelector(`[data-operator="${id}"]`);
 
-    currentCoef.innerHTML =
-        value != '' && !isNaN(value) ? Math.abs(value) : coefficient;
-    operator.innerHTML = value < 0 ? '-' : '+';
+  currentCoef.innerHTML =
+    value !== '' && !Number.isNaN(value) ? Math.abs(value) : coefficient;
+  operator.innerHTML = value < 0 ? '-' : '+';
 }
 
 function showAnswer(roots) {
-    let text = '';
+  let text = '';
 
-    if (roots.length == 1) {
-        text = `There is one root: ${roots[0]}.`;
-    } else if (roots.length == 2) {
-        text = `There are two roots: ${roots[0]} and ${roots[1]}.`;
-    } else {
-        text = 'There are no real roots.';
-    }
+  if (roots.length === 1) {
+    text = `There is one root: ${roots[0]}.`;
+  } else if (roots.length === 2) {
+    text = `There are two roots: ${roots[0]} and ${roots[1]}.`;
+  } else {
+    text = 'There are no real roots.';
+  }
 
-    answerField.innerHTML = text;
-}
-
-function clearInputs() {
-    inputsContainer
-        .querySelectorAll('.input')
-        .forEach((input) => (input.value = ''));
+  answerField.innerHTML = text;
 }
 
 inputsContainer.addEventListener('input', ({ target }) => {
-    if (target.id == 'coef-a') {
-        showAInFormula(target.value);
-    } else {
-        showOtherThanAInFormula(target.value, target.id, target.dataset.coef);
-    }
+  if (target.id === 'coef-a') {
+    showAInFormula(target.value);
+  } else {
+    showOtherThanAInFormula(target.value, target.id, target.dataset.coef);
+  }
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const a = inputsContainer.querySelector('#coef-a').value;
+  const b = inputsContainer.querySelector('#coef-b').value;
+  const c = inputsContainer.querySelector('#coef-c').value;
+  showAnswer(solveEquation(a, b, c));
 });
